580d0dc643651f4a598d0a0db69beea262c9ce21
i.giedraityte <i.giedraityte@AZU-PC1SC2Z4>
2023-11-27 19:47:27 +0200

    Add project files.

---
+++ Shopify/Models/ShopItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopify.Models
+{
+    public class ShopItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Tag { get; set; }
+        public DateTime TagExpiry { get; set; }
+        public double Price { get; set; }
+        public DateTime ItemExpiry { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Desc: {Description} Tag: {Tag} Expiry Date: {TagExpiry}" +
+                $" Price: {Price} Item expiry date: {ItemExpiry}";
+        }
+
+
+
+
+    }
+}
---
+++ Shopify/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Shopify.Models;
+
+
+List<string> items = new List<string>();
+
+var item1 = new ShopItem()
+{
+    Name = "kėdė",
+    Description = "su akcija, bet gera",
+    Tag = "svetainės",
+    TagExpiry = DateTime.Now.AddDays(2),
+    Price = 12.25,
+    ItemExpiry = DateTime.Now.AddDays(5),
+};
+
+var item2 = new ShopItem()
+{
+    Name = "sausainiai",
+    Description = "su apelsinų įdaru",
+    Tag = "saldus",
+    TagExpiry = DateTime.Now.AddDays(2),
+    Price = 0.79,
+    ItemExpiry = DateTime.Now.AddDays(5),
+};
+
+items.Add(item1.ToString());
+items.Add(item2.ToString());
+
+//foreach (var item in items)
+//{
+//    Console.WriteLine(item.ToString());
+//}
+
+File.WriteAllLines("WriteFile.txt", items);
